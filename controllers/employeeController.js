55633dd453b56578e9e588f923473e76abc4f2a0
Vikas Pathak <123vikas.vp79@gmail.com>
2023-04-30 15:34:37 +0530

    check assignment test case

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,37 +1,52 @@
-const Employee = require('../models/employeeModel');
+const Employee = require("../models/employeeModel");
 
 //Registering Employee into database
 const createEmployee = async (req, res) => {
   try {
-    // Write a code here to store Employee data
+    const employee = new Employee(req.body);
+    const savedEmployee = await employee.save();
+    res.status(201).json(savedEmployee);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to create employee' });
+    res.status(500).json({ error: "Failed to create employee" });
   }
 };
 
 //Get Employee From a Particular id
 const getEmployee = async (req, res) => {
   try {
-    // Write a code here to get Employee from a Particular id
+    const employee = await Employee.findById(req.params.id);
+    if (!employee) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+    res.json(employee);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to get employee details' });
+    res.status(500).json({ error: "Failed to get employee details" });
   }
 };
 
 //Updating Employee
 const updateEmployee = async (req, res) => {
   try {
-    //Write a code here for updating Employee details using 'PUT' request
+    const employee = await Employee.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!employee) {
+      return res.status(404).json({ error: "Employee not found" });
+    }
+    res.json(employee);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to update employee details' });
+    res.status(500).json({ error: "Failed to update employee details" });
   }
 };
 
 const deleteEmployee = async (req, res) => {
   try {
-    //Write a code here for Deleting all the employees whose salary is greater than 10000
+    const deletedEmployees = await Employee.deleteMany({
+      salary: { $gt: 10000 },
+    });
+    res.json(deletedEmployees);
   } catch (err) {
-    res.status(500).json({ error: 'Failed to delete employees' });
+    res.status(500).json({ error: "Failed to delete employees" });
   }
 };
 
--- controllers/queryController.js
+++ controllers/queryController.js
@@ -1,15 +1,24 @@
-const Employee = require('../models/employeeModel');
+const Employee = require("../models/employeeModel");
 
 const filterQueries = async (req, res) => {
+  //Write your code here for sorting & pagination
+  //1) For sorting sort salary from ascending to descending order
+  //2) For Pagination set limit 5 as a default limit and default page is 1
+  // Formulae to implementing pagination: (page - 1) * limit
+  // For Sorting use    .sort('salary')
+  const { page = 1, limit = 10, sortBy = "salary" } = req.query;
+
   try {
-    //Write your code here for sorting & pagination
-    //1) For sorting sort salary from ascending to descending order
-    //2) For Pagination set limit 5 as a default limit and default page is 1
-    // Formulae to implementing pagination: (page - 1) * limit
-    // For Sorting use    .sort('salary')
+    const employees = await Employee.find({ salary: { $gt: 10000 } })
+      .sort(sortBy)
+      .limit(parseInt(limit))
+      .skip((page - 1) * limit);
+    res.status(200).json({
+      employees
+    });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(500).json({ error: "Internal Server Error" });
   }
 };
 
